c33d1e48e8199c9fc37b4bd3a17301738b192214
Martin <martinthelindberg@gmail.com>
2020-04-11 22:09:06 -0700

    Handle <kbd> style overrides

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,34 +2,36 @@
 import { jsx } from "theme-ui"
 
 export default ({ variant }) => {
+  const overrideStyles = {
+    fontSize: "14px !important",
+    mx: 1,
+  }
+
   const interactiveStyles = {
     "&:hover": {
       bg: "#fafafa",
     },
     "&:active": {
       transform: "translateX(.5px) translateY(.5px)",
-      boxShadow: `inset 0.25px 0.25px .5px 0.25px hsl(0, 0%, 98%), inset -0.5px -0.5px 0 0.5px hsl(0, 0%, 100%),
-0 0 0 1px hsl(0, 0%, 85%), -1px -1px 0 1px hsl(0, 0%, 98%);`,
+      boxShadow: `inset 0.25px 0.25px .5px 0.25px hsl(0, 0%, 98%),
+                  inset -0.5px -0.5px 0 0.5px hsl(0, 0%, 100%),
+                  0 0 0 1px hsl(0, 0%, 85%), -1px -1px 0 1px hsl(0, 0%, 98%);`,
     },
   }
   if (!variant) {
     return (
-      <div sx={{ fontSize: "14px !important" }}>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>
-          ⌘ Cmd
-        </kbd>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>
-          ⇧ Shift
-        </kbd>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>C</kbd>
+      <div>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>⌘ Cmd</kbd>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>⇧ Shift</kbd>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>C</kbd>
       </div>
     )
   } else if (variant === "small") {
     return (
       <div>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>⌘</kbd>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>⇧</kbd>
-        <kbd sx={{ fontSize: "14px !important", ...interactiveStyles }}>C</kbd>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>⌘</kbd>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>⇧</kbd>
+        <kbd sx={{ ...overrideStyles, ...interactiveStyles }}>C</kbd>
       </div>
     )
   } else {
